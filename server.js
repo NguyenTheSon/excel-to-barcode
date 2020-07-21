fb0321eea0f20d87cabaf21d4563b6d3f64304a0
Thế Sơn <nguyentheson2711@gmail.com>
2020-07-21 21:01:16 +0700

    update code

--- server.js
+++ server.js
@@ -7,22 +7,25 @@ const document = new DOMImplementation().createDocument('http://www.w3.org/1999/
 const svgNode = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 var XLSX = require('xlsx');
 
-var workbook = XLSX.readFile('Serialforprint.xlsx');
+var workbook = XLSX.readFile('Serials300box20072020.xls');
 var sheet_name_list = workbook.SheetNames;
 var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
 
 for (let barcode of xlData) {
-  JsBarcode(svgNode, barcode['SERIAL'], {
+  JsBarcode(svgNode, barcode['Key'], {
     xmlDocument: document,
     // displayValue: false,
+    textPosition: "top",
+    width: 3.31,
+    height: 83
   });
-  
+
   let xml = xmlSerializer.serializeToString(svgNode);
   if (!fs.existsSync('./svg/')) {
     fs.mkdirSync('./svg/', { recursive: true });
   }
-  fs.writeFile(`./svg/${barcode['SERIAL']}.svg`, xml, (err) => {  
+  fs.writeFile(`./svg/${barcode['Key']}.svg`, xml, (err) => {
     if (err) throw err;
-    console.log(`SVG written! ${barcode['SERIAL']}`);
+    console.log(`SVG written! ${barcode['Key']}`);
   });
 }
